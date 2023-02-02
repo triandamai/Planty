c958b07e81a2143af9462725447bb98a49df3457
qamarelsafadi <qamar.elsafadi98@gmail.com>
2023-02-03 00:01:25 +0200

    feat: update model with properties

--- app/src/main/java/com/qamar/planty/data/model/Plant.kt
+++ app/src/main/java/com/qamar/planty/data/model/Plant.kt
@@ -1,8 +1,31 @@
 package com.qamar.planty.data.model
 
- class Plant {
 
-     fun getPlantList(): List<String> {
-         return listOf("Bird of paradise", "Dracaena", "Palm", "Ficus", "Cedar", "Fiddle")
-     }
+data class Plant(
+    val name: String? = "",
+    val properties: List<Properties> =
+        listOf(
+            Properties("Light"),
+            Properties("Food"),
+            Properties("Water"),
+        ),
+) {
+
+    fun getPlantList(): List<Plant> {
+
+        //"Bird of paradise", "Dracaena", "Palm", "Ficus", "Cedar", "Fiddle"
+        return listOf(
+            Plant("Bird of paradise"),
+            Plant("Dracaena"),
+            Plant("Palm"),
+            Plant("Ficus"),
+            Plant("Cedar"),
+            Plant("Fiddle"),
+        )
+    }
 }
+
+data class Properties(
+    val propertyName: String,
+    val percent: Float = (0 until 100).random().toFloat()/100
+)
